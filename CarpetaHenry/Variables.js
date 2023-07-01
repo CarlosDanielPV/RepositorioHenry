6f20b6abce2d46fb18541efa4669bd5af7a32dc9
Carlos Daniel Prieto Vera <137470890+CarlosDanielPV@users.noreply.github.com>
2023-07-01 00:02:40 -0500

    Entendiendo variables

---
+++ CarpetaHenry/Variables.js
@@ -0,0 +1,22 @@
+// ENTENDIENDO VARIABLES
+
+//variable de tipo String o texto
+var cajon1;
+cajon1 = 'Hola aprendiz';
+var cajon = 'Holas';
+
+//variable de tipo numerico
+var numeroEntero = 33;
+var numeroNegativo = -9;
+var numeroDecimal = 65.32;
+
+//variable Boolean
+var esCierto = true;
+var esFalso = false;
+
+//variable sin definir
+var cajaVacia;
+console.log(cajaVacia);
+
+//variable vacia o nula
+var sinNada = null;
